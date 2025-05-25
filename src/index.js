b05e1a1823f149bcd99c65949c8a375158000e1b
shahwazzz <shahwazsid777@gmail.com>
2025-05-26 00:06:06 +0530

    Add cookie-parser and cors middleware, implement ApiError and ApiResponse classes, and create asyncHandler utility

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config({
-    path: './.env'
-})
-
-
+  path: "./.env",
+});
 
 connectDB()
-
-
-
-
-
-
-
-
-
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
 
 /*
 import express from "express"
